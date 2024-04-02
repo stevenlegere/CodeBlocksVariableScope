025b0373872c7dcbef3dff842ad285aaf8c0e48c
stevenlegere <stevenmarklegere@gmail.com>
2024-04-02 21:35:57 +0100

    working with scope

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
-﻿bool flag = true;
-int value = 0; // initialise the varioable value
-if (flag)
-{
-    Console.WriteLine($"Inside the code block: {value}");
-}
+﻿// bool flag = true;
+// int value = 0; // initialise the varioable value
+// if (flag)
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
 
-value = 10;
-Console.WriteLine($"Outside the code block: {value}");
+// value = 10;
+// Console.WriteLine($"Outside the code block: {value}");
+
+// bool flag = true;
+// if (flag)
+// {
+//     Console.WriteLine(flag);
+// }
+// // true
+
+// bool flag = true;
+// if (flag)
+//     Console.WriteLine(flag);
+
+bool flag = true;
+if (flag) Console.WriteLine(flag);
+
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "Steve")
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
+
+
+
+
